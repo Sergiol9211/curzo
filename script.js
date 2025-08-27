8882a935c4620a0835c6a007972627b1e8e73fc9
Sergiol9211 <liberato11@hotmail.com>
2025-08-27 15:02:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slider img");
+    let index = 0;
+
+    function showNextImage() {
+        slides[index].classList.remove("active");   // ocultar actual
+        index = (index + 1) % slides.length;        // siguiente
+        slides[index].classList.add("active");      // mostrar siguiente
+    }
+
+    setInterval(showNextImage, 3000); // cambiar cada 5s
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slider2 img");
+    let index = 0;
+
+    function showNextImage() {
+        slides[index].classList.remove("active");   // ocultar actual
+        index = (index + 1) % slides.length;        // siguiente
+        slides[index].classList.add("active");      // mostrar siguiente
+    }
+
+    setInterval(showNextImage, 3000); // cambiar cada 5s
+});
